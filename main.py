9337653bfd10aaed034247097948614447c96f6a
Brandon Colangelo <brandoncancode@gmail.com>
2023-09-22 08:44:59 -0400

    Drawing the game grid.

--- main.py
+++ main.py
@@ -8,13 +8,40 @@ from tkinter import messagebox
 # Classes
 
 # Functions
-# Main game loop
+# Draws the game grid.
+def draw_grid(w, rows, surface):
+    size_between = w // rows
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + size_between
+        y = y + size_between
+
+        pygame.draw.line(surface, (255, 255, 255), (x,0), (x, w))
+        pygame.draw.line(surface, (255, 255, 255), (0,y), (w, y))
+
+# Redraws all objects on the playing window.
+def redraw_window(surface):
+    global rows, width
+    win.fill((0, 0, 0))
+    draw_grid(width, rows, surface)
+    pygame.display.update()
+
+# Main game loop.
 def main():
-    WIDTH = 500
-    HEIGHT = 500
+    global width, rows
+    width = 500
     rows = 20
-    win = pygame.display.set_mode((WIDTH, HEIGHT))
-    snake = snake(())
+    win = pygame.display.set_mode((width, width))
+    snake = snake((255, 0, 0), (10, 10))
+    flag = True
+
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        redraw_window(win)
 
 
 main()
