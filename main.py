4a1e39111238acf213a1413b1173f1767c535842
Brandon Colangelo <brandoncancode@gmail.com>
2023-09-22 08:32:03 -0400

    Initial commit.

---
+++ main.py
@@ -0,0 +1,20 @@
+# Imports.
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+# Classes
+
+# Functions
+# Main game loop
+def main():
+    WIDTH = 500
+    HEIGHT = 500
+    rows = 20
+    win = pygame.display.set_mode((WIDTH, HEIGHT))
+    snake = snake(())
+
+
+main()
